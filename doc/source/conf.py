40fbe61481396af5c000621138485ecf1c0fb37a
BJ Dierkes <derks@datafolklabs.com>
2014-09-25 13:31:35 -0500

    Update Doc Theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,6 +13,13 @@
 
 import sys, os
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # If we dont' prep an app, then we'll get runtime errors
 from cement.utils import test
 fakeapp = test.TestApp()
@@ -99,7 +106,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+#html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
