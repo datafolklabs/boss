a7a403bf55d404607ac9c988d684fbf772e08cc2
BJ Dierkes <derks@datafolklabs.com>
2014-12-05 21:17:16 -0600

    Adding tests/templates

---
+++ tests/templates/python/tests/test_@module@.py
@@ -0,0 +1,16 @@
+"""Tests for @project@."""
+
+import unittest
+#from nose.tools import ok_, eq_, raises
+#from nose import SkipTest
+    
+class @module.capitalize@TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+        
+    def test_@module@(self):
+        # do something to test @module@
+        pass
